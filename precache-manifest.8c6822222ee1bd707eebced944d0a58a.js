e42e53b5d50e96864674b9e2befa314bb12bd307
dennis-johnson <densarjohn@gmail.com>
2019-10-07 01:14:42 +0530

    Updates

---
+++ precache-manifest.8c6822222ee1bd707eebced944d0a58a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0628143451a45ce140b24e68710daf",
+    "url": "/tape/index.html"
+  },
+  {
+    "revision": "da9be207b7439999da33",
+    "url": "/tape/static/css/main.60b7f7ce.chunk.css"
+  },
+  {
+    "revision": "90da55548b2e7e3407c0",
+    "url": "/tape/static/js/2.19f65e29.chunk.js"
+  },
+  {
+    "revision": "da9be207b7439999da33",
+    "url": "/tape/static/js/main.3e79a200.chunk.js"
+  },
+  {
+    "revision": "704a50606314b9a19816",
+    "url": "/tape/static/js/runtime~main.1c655dd9.js"
+  },
+  {
+    "revision": "1bcaf4d863817f00e02a61ed49a92f62",
+    "url": "/tape/static/media/appleIconWhite.1bcaf4d8.svg"
+  },
+  {
+    "revision": "428d2d447cefa7e77f2bb733d4c2071e",
+    "url": "/tape/static/media/cassette-tape.428d2d44.png"
+  },
+  {
+    "revision": "d3dfb9baf290cb72e02e2ab8ee4ccfc2",
+    "url": "/tape/static/media/dennis.d3dfb9ba.jpg"
+  },
+  {
+    "revision": "b818d523d8560f33bcf5d6628f0ffa73",
+    "url": "/tape/static/media/spotifyIcon.b818d523.png"
+  }
+]);
